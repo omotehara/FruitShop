8e4d43318ae49d44efd424a101090392956a5ec1
Yumi Omotehara <oca2602it0004@edu.oca.ac.jp>
2026-07-24 06:18:41 +0000

    Initial commit (Rails setup)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,27 @@
 Rails.application.routes.draw do
+  # 新規商品
+  get "products/new"# 商T品投稿フォームを表示
   #get "homes/top"
+  post 'products', to: 'products#create'  # 登録
+
+  # 商品一覧
+  get 'products', to: 'products#index'
+  
+  # 商品詳細
+  get 'products/:id', to: 'products#show', as: 'product'
+
+  # 商品編集
+  get 'products/:id/edit', to: 'products#edit', as: 'edit_product'
+  patch 'products/:id', to: 'products#update' # 編集
+
+  # 商品削除
+  delete 'products/:id', to: 'products#destroy', as: 'destroy_product'
+
+
+  # トップページ
   root to: "homes#top"
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
--- db/migrate/20260717065544_create_products.rb
+++ db/migrate/20260717065544_create_products.rb
@@ -1,9 +1,11 @@
 class CreateProducts < ActiveRecord::Migration[8.1]
   def change
     create_table :products do |t|
-      t.string :name
+      #t.string :name
+      t.string :name, null: false    # 商品名（必須）
       t.text :description
-      t.integer :price
+      #t.integer :price
+      t.integer :price, null: false  # 価格（必須）
 
       t.timestamps
     end
