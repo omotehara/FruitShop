de268dd09488fbc0d1ae0b94a2802a1a098173e6
Yumi Omotehara <oca2602it0004@edu.oca.ac.jp>
2026-09-11 04:59:23 +0000

    7-8実装終了

--- config/routes.rb
+++ config/routes.rb
@@ -5,24 +5,8 @@ Rails.application.routes.draw do
   # マイページ
   resources :mypage, only: [:show]
 
-  # 新規商品
-  get "products/new"# 商T品投稿フォームを表示
-  #get "homes/top"
-  post 'products', to: 'products#create'  # 登録
-
-  # 商品一覧
-  get 'products', to: 'products#index'
-  
-  # 商品詳細
-  get 'products/:id', to: 'products#show', as: 'product'
-
-  # 商品編集
-  get 'products/:id/edit', to: 'products#edit', as: 'edit_product'
-  patch 'products/:id', to: 'products#update' # 編集
-
-  # 商品削除
-  delete 'products/:id', to: 'products#destroy', as: 'destroy_product'
-
+  # 商品関連
+  resources :products
 
   # トップページ
   root to: "homes#top"
